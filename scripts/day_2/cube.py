b761d70c67368c2f865576ded499313d78779194
Ronin Wu <roninwu@gmail.com>
2023-12-02 17:57:39 +0100

    Improve game_info dictionary
    In the previous commit, the "draw_info" dictionary in the game_info is
    not correct as each draw_info dictionary contains only one color at a
    time. This commit fixes it so that the resulting number of each color in
    every draw is recorded simutanously.

--- scripts/day_2/cube.py
+++ scripts/day_2/cube.py
@@ -4,7 +4,7 @@ from typing import Dict, List
 
 SCRIPT_PATH = Path(__file__).parent.absolute()
 
-GAME_ID_REGEX = re.compile(r"(?P<game_name>(?<=Game )\d+(?=:))")
+GAME_ID_REGEX = re.compile(r"(?:Game (?P<game_name>\d+): )")
 GAME_INFO_REGEX = re.compile(
     r"(?P<blue>(\d+(?= blue)))|"
     r"(?P<red>(\d+(?= red)))|"
@@ -18,17 +18,27 @@ GAME_CONSTRAINT = {
 ALL_COLORS = list(GAME_CONSTRAINT.keys())
 
 
-def game_string_to_game_info(game_string: str) -> Dict[int, List[Dict[str, int]]]:
+def get_draw_info(draw_string: str) -> Dict[str, int]:
+    draw = dict.fromkeys(ALL_COLORS, 0)
+    for color in ALL_COLORS:
+        for m in GAME_INFO_REGEX.finditer(draw_string):
+            if m.group(color):
+                draw[color] = int(m.group(color))
+    return draw
+
+
+def get_game_string_to_game_info(game_string: str) -> Dict[int, List[Dict[str, int]]]:
     game_info = {}
-    game_id = int(GAME_ID_REGEX.search(game_string).group())
-    game_info[game_id] = [
-        {k: int(v) if v else 0 for k, v in m.groupdict().items()}
-        for m in GAME_INFO_REGEX.finditer(game_string)
-        ]
+    game_id_match = GAME_ID_REGEX.search(game_string)
+    game_id = int(game_id_match.group("game_name"))
+    draw_info_list = []
+    for draw_string in game_string[game_id_match.end():].split(";"):
+        draw_info_list.append(get_draw_info(draw_string))
+    game_info[game_id] = draw_info_list
     return game_info
 
 
-def draw_is_possible(one_draw: Dict[str, int]) -> bool:
+def is_draw_possible(one_draw: Dict[str, int]) -> bool:
     return all(
         one_draw[k] <= GAME_CONSTRAINT[k]
         for k in GAME_CONSTRAINT.keys()
@@ -37,7 +47,7 @@ def draw_is_possible(one_draw: Dict[str, int]) -> bool:
 
 def is_game_possible(game_info: Dict[int, List[Dict[str, str]]]) -> bool:
     return all(
-        draw_is_possible(one_draw)
+        is_draw_possible(one_draw)
         for one_draw in list(game_info.values())[0]
         )
 
@@ -55,7 +65,7 @@ def derive_game_power(game_info: Dict[int, List[Dict[str, str]]]) -> int:
 
 def main():
     with open(SCRIPT_PATH / "input.dat", "r") as f:
-        games = [game_string_to_game_info(line.strip("\n")) for line in f.readlines()]
+        games = [get_game_string_to_game_info(line.strip("\n")) for line in f.readlines()]
     possible_games = [game for game in games if is_game_possible(game)]
     print(f"Answer to part 1: {sum(list(game.keys())[0] for game in possible_games)}")
     game_powers = [derive_game_power(game) for game in games]
