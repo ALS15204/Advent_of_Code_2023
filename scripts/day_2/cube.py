12d6e805bad57cb43799678d9527e67a54d97921
Ronin Wu <roninwu@gmail.com>
2023-12-02 18:11:09 +0100

    Rename variables and functions for clarity

--- scripts/day_2/cube.py
+++ scripts/day_2/cube.py
@@ -22,24 +22,24 @@ GAME_CONSTRAINT = {
 ALL_COLORS = list(GAME_CONSTRAINT.keys())
 
 
-def get_draw_info(draw_string: str) -> Dict[str, int]:
-    draw = dict.fromkeys(ALL_COLORS, 0)
+def get_color_to_number_per_draw(draw_string: str) -> Dict[str, int]:
+    color_to_number_per_draw = dict.fromkeys(ALL_COLORS, 0)
     for color in ALL_COLORS:
         for m in GAME_INFO_REGEX.finditer(draw_string):
             if m.group(color):
-                draw[color] = int(m.group(color))
-    return draw
+                color_to_number_per_draw[color] = int(m.group(color))
+    return color_to_number_per_draw
 
 
-def get_game_string_to_game_info(game_string: str) -> Dict[int, List[Dict[str, int]]]:
-    game_info = {}
+def get_game_id_to_draws(game_string: str) -> Dict[int, List[Dict[str, int]]]:
+    game_id_to_draws = {}
     game_id_match = GAME_ID_REGEX.search(game_string)
     game_id = int(game_id_match.group(GAME_ID_STRING))
     draw_info_list = []
     for draw_string in game_string[game_id_match.end():].split(";"):
-        draw_info_list.append(get_draw_info(draw_string))
-    game_info[game_id] = draw_info_list
-    return game_info
+        draw_info_list.append(get_color_to_number_per_draw(draw_string))
+    game_id_to_draws[game_id] = draw_info_list
+    return game_id_to_draws
 
 
 def is_draw_possible(one_draw: Dict[str, int]) -> bool:
@@ -69,7 +69,7 @@ def derive_game_power(game_info: Dict[int, List[Dict[str, str]]]) -> int:
 
 def main():
     with open(SCRIPT_PATH / "input.dat", "r") as f:
-        games = [get_game_string_to_game_info(line.strip("\n")) for line in f.readlines()]
+        games = [get_game_id_to_draws(line.strip("\n")) for line in f.readlines()]
     possible_games = [game for game in games if is_game_possible(game)]
     print(f"Answer to part 1: {sum(list(game.keys())[0] for game in possible_games)}")
     game_powers = [derive_game_power(game) for game in games]
